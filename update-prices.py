412b94417df9b135b705ba83d64fcf00cabd396e
Nectariferous <likhonexe@gmail.com>
2024-07-09 07:26:28 +0600

    Update update-prices.py

--- update-prices.py
+++ update-prices.py
@@ -1,26 +1,34 @@
 import requests
 import json
 
-url = "https://price.api.cx.metamask.io/v1/exchange-rates?baseCurrency=usd"
-response = requests.get(url)
-data = response.json()
+# Fetch cryptocurrency prices
+price_url = "https://price.api.cx.metamask.io/v1/exchange-rates?baseCurrency=usd"
+price_response = requests.get(price_url)
+price_data = price_response.json()
 
-prices = {
-    "btc": data["btc"]["value"],
-    "eth": data["eth"]["value"],
-    "ltc": data["ltc"]["value"]
-}
+# Fetch donation balances
+donation_url = "https://accounts.api.cx.metamask.io/v2/accounts/0x3a06322e9f1124f6b2de8f343d4fdce4d1009869/balances?networks=1%2C10%2C56%2C137%2C8453%2C59144&includeUnverifiedAssets=true&filterSupportedTokens=true"
+donation_response = requests.get(donation_url)
+donation_data = donation_response.json()
 
-with open("README.md", "r") as file:
-    readme = file.readlines()
+# Create README.md content
+readme_content = f"""# Cryptocurrency Prices
 
-for i, line in enumerate(readme):
-    if line.startswith("![Bitcoin Price]"):
-        readme[i] = f"![Bitcoin Price](https://img.shields.io/badge/Bitcoin-{prices['btc']}-btc.svg?style=flat-square)\n"
-    elif line.startswith("![Ethereum Price]"):
-        readme[i] = f"![Ethereum Price](https://img.shields.io/badge/Ethereum-{prices['eth']}-eth.svg?style=flat-square)\n"
-    elif line.startswith("![Litecoin Price]"):
-        readme[i] = f"![Litecoin Price](https://img.shields.io/badge/Litecoin-{prices['ltc']}-ltc.svg?style=flat-square)\n"
+![Bitcoin Price](https://img.shields.io/badge/Bitcoin-{price_data['btc']['value']}-btc.svg?style=flat-square)
+![Ethereum Price](https://img.shields.io/badge/Ethereum-{price_data['eth']['value']}-eth.svg?style=flat-square)
+![Litecoin Price](https://img.shields.io/badge/Litecoin-{price_data['ltc']['value']}-ltc.svg?style=flat-square)
 
+# Donation Trackers
+
+![ETH Donations](https://img.shields.io/badge/ETH-{donation_data['balances'][1]['balance']}-eth.svg?style=flat-square)
+![BNB Donations](https://img.shields.io/badge/BNB-{donation_data['balances'][6]['balance']}-bnb.svg?style=flat-square)
+
+# Live Updates from Telegram
+
+<div class="telegram-post-widget" data-telegram-post="Telegram/5"></div>
+<script async src="https://telegram.org/js/telegram-widget.js?14" data-telegram-post="Telegram/5"></script>
+"""
+
+# Save to README.md file
 with open("README.md", "w") as file:
-    file.writelines(readme)
+    file.write(readme_content)
